686587f450ce38ccb33ef7728433c745c07e80db
JosephChoww <mingjunchow1711@gmail.com>
2022-10-15 15:30:32 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.\n")
+a = input("What is the name of the city you grew up in?\n")
+b = input("What is a name of pet you had?\n")
+print('your new band name could be '+a+" "+b)
